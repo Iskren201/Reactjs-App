828a3c4a1f64c84b44dd017dff598773a72b2b66
Iskren201 <iskren201@gmail.com>
2023-09-18 13:01:53 +0300

    WeatherApp created and style

---
+++ weather-app/src/Components/WeatherApp/WeatherApp.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './WeatherApp.css';
+
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
+
+const WeatherApp = () => {
+    return (
+        <div className="container">
+            <div className="top-bar">
+                <input type="text" className="cotyInput" placeholder="Search" />
+                <div className="search-icon">
+                    <img src={search_icon} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WeatherApp;
