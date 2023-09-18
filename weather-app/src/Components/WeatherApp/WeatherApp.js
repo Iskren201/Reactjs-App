8589061e2840ca63e7b154987757ad8930cdc8de
Iskren201 <iskren201@gmail.com>
2023-09-18 13:30:36 +0300

    API , URL add

--- weather-app/src/Components/WeatherApp/WeatherApp.js
+++ weather-app/src/Components/WeatherApp/WeatherApp.js
@@ -11,13 +11,46 @@ import wind_icon from '../Assets/wind.png';
 import humidity_icon from '../Assets/humidity.png';
 
 const WeatherApp = () => {
+
+    let api_key = 'e474228e3257e72a79a0a1953c8d4175';
+    const search = () => {
+        const element = document.getElementsByClassName("cityInput");
+        if (element[0].value === "") {
+            return 0;
+        }
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=e474228e3257e72a79a0a1953c8d4175";
+
+    }
+
     return (
         <div className="container">
             <div className="top-bar">
-                <input type="text" className="cotyInput" placeholder="Search" />
-                <div className="search-icon">
+                <input type="text" className="cityInput" placeholder="Search" />
+                <div className="search-icon" onClick={() => { search() }}>
                     <img src={search_icon} />
                 </div>
+            </div>
+            <div className="weather-image">
+                <img src={cloud_icon} />
+            </div>
+            <div className="weather-temp">24 c</div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percent">18 km/h</div>
+                        <div className="text">Winds Speed</div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
